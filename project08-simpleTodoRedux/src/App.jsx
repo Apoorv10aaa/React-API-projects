42717f1b6c5ebaec1f4d9ad8c432737de33cfa63
Apoorv Srivastava <apoorvsrivastava2121@gmail.com>
2024-04-15 17:06:14 +0530

    todoWithReduxToolKit

---
+++ project08-simpleTodoRedux/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+
+  return (
+    <>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
